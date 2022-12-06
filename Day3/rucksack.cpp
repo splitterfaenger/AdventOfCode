797749bbd8552bdb0c51550f9ab3ba45045e829d
splitterfaenger <sven.rowald@posteo.de>
2022-12-06 21:35:27 +0100

    Day3 dome

--- Day3/rucksack.cpp
+++ Day3/rucksack.cpp
@@ -16,21 +16,38 @@ int main() {
 
     int duplicates_count{};
 
-    std::ifstream file("test.txt");
+    std::ifstream file("rucksacks.txt");
     std::string line{};
+    int counter{0};
+    std::string storage{};
     while (std::getline(file, line)) {
-        std::vector<char> storage{};
-        if (!line.empty()) {
-            for (int i = 0; i <= line.size()/2  ; i++) {
-                storage.push_back(line[i]);
-            }
-            auto middle_of_line = line.begin() + line.size()/2;
-            for (char c : storage) {
-
-                if (std::find(middle_of_line, line.end(), c) != line.end()) {
-                    int char_store{};
-                    char_store = *std::find(middle_of_line, line.end(), c);
-                    duplicates_count += char_store >= 97 ? char_store - 96 : char_store - 38 ;
+        std::sort(line.begin(), line.end());
+        line.erase(std::unique(line.begin(), line.end()), line.end());
+        storage += line;
+        counter++;
+        if (counter ==  3) {
+            counter = 0;
+            std::sort(storage.begin(), storage.end());
+            char b{' '};
+            int triple_counter{0};
+            for (char c: storage) {
+                if (b == c) {
+                    triple_counter++;
+                }
+                if ( b != c) {
+                    triple_counter = 0;
+                }
+                b = c;
+                if (triple_counter == 2) {
+                    triple_counter = 0;
+                    if (c >= 'a' && c <= 'z') {
+                        duplicates_count += c - 'a' + 1;
+                    }
+                    if (c >= 'A' && c <= 'Z') {
+                        duplicates_count += c - 'A' + 27;
+                    }
+                    storage.clear();
+                    break;
                 }
             }
         }
